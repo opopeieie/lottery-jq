bdc063e3eb13cdce25e525502cf1d8542d154d3a
LIANGQING <453225091@qq.com>
2018-12-16 21:26:07 +0800

    change code

--- helper.js
+++ helper.js
@@ -1,13 +1,23 @@
-var fs = require('fs');
-var staticPath = './photo';
-var photoDir = fs.readdirSync(staticPath);
-var array = []
-for(var photo of photoDir) {
-    var obj = {};
-	obj.name = photo.split(photo[photo.lastIndexOf('.')])[0];
-	obj.path = staticPath+'/'+photo;
-    array.push(obj);
+const fs = require('fs');
+const array = [];
+
+function makeJson(path) {
+    const fileDir = fs.statSync(path);
+    if(fileDir.isDirectory()) {
+        const dir = fs.readdirSync(path);
+        for(const i of dir) {
+            makeJson(path+'/'+i);
+        }
+    }else {
+        const obj = {};
+        const allName = path.split(path[path.lastIndexOf('.')])[0];
+        const realName = allName.split(allName[allName.lastIndexOf('/')]);
+        obj.name = realName[realName.length - 1];
+        obj.path = path;
+        array.push(obj);
+    }
 }
 
-fs.writeFileSync('config.json', JSON.stringify(array));
+makeJson('年会抽奖');
 
+fs.writeFileSync('config.json', JSON.stringify(array));
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     $.getJSON('./config.json').then(function(array) {
-        for(const i in array) {
-            const data = array[i];
-            $('#root').append('<div><img class="user-photo" src='+data.path+'/><span>name:'+data.name+'</span></div>')
-        }
+        // for(const i in array) {
+        //     const data = array[i];
+        //     $('#root').append('<div><img class="user-photo" src="'+String(data.path)+'" /><span>name:'+data.name+'</span></div>')
+        // }
     })
 }
